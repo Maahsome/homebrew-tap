721ec9123ba1606b0c9fb465e07f0fc388f3759d
Christopher Maahs <cmaahs@gmail.com>
2021-09-07 04:23:39 -0500

    Brew formula update for gitlab-tool version v0.0.3

--- Formula/gitlab-tool.rb
+++ Formula/gitlab-tool.rb
@@ -5,24 +5,24 @@
 class GitlabTool < Formula
   desc "CLI tool to reduce reliance on GUI interactions, initially driven by wanting to grep through TRACE output of pipeline jobs"
   homepage "https://github.com/maahsome/gitlab-tool/"
-  version "0.0.2"
+  version "0.0.3"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/maahsome/gitlab-tool/releases/download/v0.0.2/gitlab-tool_darwin_amd64.tar.gz"
-      sha256 "311a37e8b24c6e4fddf05fced29ffff4f0c90e50ede37611a183ffd8662f2245"
+      url "https://github.com/maahsome/gitlab-tool/releases/download/v0.0.3/gitlab-tool_darwin_amd64.tar.gz"
+      sha256 "495438db64388d98ad71a6aca2744791d2352fb957d5b3ed59a910fa37f5019e"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/maahsome/gitlab-tool/releases/download/v0.0.2/gitlab-tool_linux_amd64.tar.gz"
-      sha256 "d84ab149583d6936e63407733d9c28a67c9dc8a1051a7e625fa0c9e179e3b31b"
+      url "https://github.com/maahsome/gitlab-tool/releases/download/v0.0.3/gitlab-tool_linux_amd64.tar.gz"
+      sha256 "668573ecb6a01aa1668d54d87c67d1b58db5b93d6568ad4ed65110bf1b4025f1"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/maahsome/gitlab-tool/releases/download/v0.0.2/gitlab-tool_linux_arm64.tar.gz"
-      sha256 "b095d7a84bc940d11c3ee0327f85cb705edf9076f465d02ee666cc5333ffbb0c"
+      url "https://github.com/maahsome/gitlab-tool/releases/download/v0.0.3/gitlab-tool_linux_arm64.tar.gz"
+      sha256 "4c39e75a9735727832fe3a93ae0d963e716f323738b08036abc3cbe906d6600e"
     end
   end
 
