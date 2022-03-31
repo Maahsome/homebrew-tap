c10c03695228a7e74ec8f54868d1dfd3b14f8ba8
Christopher Maahs <cmaahs@gmail.com>
2022-03-30 20:35:23 -0500

    Brew formula update for bwca version v0.0.18

--- Formula/bwca.rb
+++ Formula/bwca.rb
@@ -5,20 +5,20 @@
 class Bwca < Formula
   desc "Bitwarden has created a SERVE mode for the CLI tool, this CLI interacts with that REST API on localhost"
   homepage "https://github.com/maahsome/bwca/"
-  version "0.0.17"
+  version "0.0.18"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/maahsome/bwca/releases/download/v0.0.17/bwca_darwin_arm64.tar.gz"
-      sha256 "e27dd28a91bdc5ea03da9bba1ba59ae9b9718084e30e1b82d99a3e815c193ac5"
+    if Hardware::CPU.intel?
+      url "https://github.com/maahsome/bwca/releases/download/v0.0.18/bwca_darwin_amd64.tar.gz"
+      sha256 "ae3a3a80eda2504f6cb1fce752be1ecb825988b8a4bf01f6d6d16c7556451a00"
 
       def install
         bin.install "bwca"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/maahsome/bwca/releases/download/v0.0.17/bwca_darwin_amd64.tar.gz"
-      sha256 "94cc9415ebb0be76ec40ec43e6b5592916247c2e1d5d2f4abca4845aacf674ac"
+    if Hardware::CPU.arm?
+      url "https://github.com/maahsome/bwca/releases/download/v0.0.18/bwca_darwin_arm64.tar.gz"
+      sha256 "508183ec4efccaac87a2be9b254e25c099c037b97cc2275c1a9a6cee577ce56e"
 
       def install
         bin.install "bwca"
@@ -28,16 +28,16 @@ class Bwca < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/maahsome/bwca/releases/download/v0.0.17/bwca_linux_arm64.tar.gz"
-      sha256 "c3bae8deffadf29a2a03f73de4477e30936ee0c9807bbc9842b74fdea5e026f8"
+      url "https://github.com/maahsome/bwca/releases/download/v0.0.18/bwca_linux_arm64.tar.gz"
+      sha256 "2507f5fb1cb3366eddb0680607c24d646fc2f406daa25da1b2fb8be4e96f66d5"
 
       def install
         bin.install "bwca"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/maahsome/bwca/releases/download/v0.0.17/bwca_linux_amd64.tar.gz"
-      sha256 "eb91aa98c9ea326b3bb0fc467a71aefa45327e7e4336acd15d247e5df9ba5d5f"
+      url "https://github.com/maahsome/bwca/releases/download/v0.0.18/bwca_linux_amd64.tar.gz"
+      sha256 "90aedfac267b05707e6351a2dd9a2c48c9399c214d36075361f7f2d3c50f736e"
 
       def install
         bin.install "bwca"
