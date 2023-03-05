50f00573d4f6593e6ecb9d42315b12ef375f9d1a
Christopher Maahs <cmaahs@gmail.com>
2023-03-05 06:36:20 -0600

    Brew formula update for ktrouble version v0.0.13

--- Formula/ktrouble.rb
+++ Formula/ktrouble.rb
@@ -5,20 +5,20 @@
 class Ktrouble < Formula
   desc "CLI tool to reduce reliance on GUI interactions, initially driven by wanting to grep through TRACE output of pipeline jobs"
   homepage "https://github.com/maahsome/ktrouble/"
-  version "0.0.10"
+  version "0.0.13"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/maahsome/ktrouble/releases/download/v0.0.10/ktrouble_darwin_arm64.tar.gz"
-      sha256 "99597a5da9a050baffb1ea9db6bd183aaf6fb5ba71962589889b2d48f2c9442d"
+      url "https://github.com/maahsome/ktrouble/releases/download/v0.0.13/ktrouble_darwin_arm64.tar.gz"
+      sha256 "bb5f3744c15683b8ecf79698efdc3fcf76fcb4dc47db99d29417e85068dc73ef"
 
       def install
         bin.install "ktrouble"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/maahsome/ktrouble/releases/download/v0.0.10/ktrouble_darwin_amd64.tar.gz"
-      sha256 "1fc9c404a87bf5be07a4711ca2165a581ecd4ebe33458cb27161ab3130975cd0"
+      url "https://github.com/maahsome/ktrouble/releases/download/v0.0.13/ktrouble_darwin_amd64.tar.gz"
+      sha256 "fe13dd2a12f2a710ae48c6f629d7e268b0991f3e861bceffeb5eebc3a9b73f51"
 
       def install
         bin.install "ktrouble"
@@ -28,16 +28,16 @@ class Ktrouble < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/maahsome/ktrouble/releases/download/v0.0.10/ktrouble_linux_amd64.tar.gz"
-      sha256 "5bdb8b0a68ab9ee8fdda9b9b840ce2ca372949f8a2a47626db609019ff20c9ee"
+      url "https://github.com/maahsome/ktrouble/releases/download/v0.0.13/ktrouble_linux_amd64.tar.gz"
+      sha256 "d8443fb300af54337521738b250317556bdc753af9b5376c96f5419f2b0aa4d9"
 
       def install
         bin.install "ktrouble"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/maahsome/ktrouble/releases/download/v0.0.10/ktrouble_linux_arm64.tar.gz"
-      sha256 "aa0630a22e4b4a420eeee8a637a9db6e34c92e37b9f5b404d6f22150ce5829d9"
+      url "https://github.com/maahsome/ktrouble/releases/download/v0.0.13/ktrouble_linux_arm64.tar.gz"
+      sha256 "5d0f2c2fe0f7cb443d80ceb1d0569efba8a816eda262c6f9ae2b0ef42c62f9bb"
 
       def install
         bin.install "ktrouble"
